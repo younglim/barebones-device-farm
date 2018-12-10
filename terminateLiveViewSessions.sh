3c594f9a09530d677fde1029cbf12c4378040d08
younglim <younglim@users.noreply.github.com>
2018-12-10 17:25:47 +0800

    Better termination of VirtualHere server and adb

--- terminateLiveViewSessions.sh
+++ terminateLiveViewSessions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-xpra stop :100 :101 :102 :103 :104 :105 :106 :107 :108
+xpra stop :100 :101 :102 :103 :104 :105 :106 :107 :108 :200
 adb kill-server
-sudo pkill vhclientx86_64
-
+sudo pkill -9 adb
+sudo pkill -9 vhclientx86_64
