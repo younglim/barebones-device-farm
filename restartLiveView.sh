92055dd3f906acaab4fa4bb7b4d27c63d8407cfc
younglim <younglim@users.noreply.github.com>
2018-12-10 17:24:37 +0800

    Change Display Options for Ubuntu compatibility

--- restartLiveView.sh
+++ restartLiveView.sh
@@ -2,17 +2,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 SLEEP=10
 
-DISPLAYOPTS="--bandwidth-limit=2M --video-scaling=100 --desktop-fullscreen=true --pixel-depth=16 --desktop-scaling=auto --window-close=ignore --speaker=disabled --microphone=disabled --pulseaudio=no"
+DISPLAYOPTS="--video-scaling=100 --desktop-fullscreen=true --pixel-depth=16 --desktop-scalin$
 
 echo "Terminating any existing live view sessions"
 $DIR/terminateLiveViewSessions.sh 
 
 echo "Restart VirtualHere connection to devices"
-$DIR/restartVhClient.sh 
+$DIR/restartVhClient.sh
 
-sleep 30
+echo "Print VirtualHere Server Log"
+cat $DIR/vhserver.log
 
 echo "Starting Live View"
+
 # Mi 3
 xpra start --bind-tcp=0.0.0.0:14500 --html=on --start="scrcpy -s MYSERIALNUMBER -m 768" $DISPLAYOPTS --daemon=no :100 &
 sleep $SLEEP
