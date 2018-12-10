4ce934e69ee13b475cf28a4ffc43c1810bf9ea5a
younglim <younglim@users.noreply.github.com>
2018-12-10 17:27:07 +0800

    Better termination of VirtualHere and adb

--- runVhui.sh
+++ runVhui.sh
@@ -1,5 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-sudo pkill vhclientx86_64
+sudo pkill -9 vhclientx86_64
+sudo pkill -9 vhuit64
 sleep 5
 
 sudo xpra start --bind-tcp=0.0.0.0:13000 --html=on --start="$DIR/vhuit64 -c $DIR/.vhui" --daemon=no :200
