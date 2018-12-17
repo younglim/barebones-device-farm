f40dc1bed7174907b00cb4a451130a9ccac7f019
younglim <younglim@users.noreply.github.com>
2018-12-17 17:26:19 +0800

    More reliable starting of VirtualHere and adb

--- restartLiveView.sh
+++ restartLiveView.sh
@@ -1,11 +1,11 @@
 # Stop all mobile device sessions
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-SLEEP=10
 
-DISPLAYOPTS="--video-scaling=100 --desktop-fullscreen=true --pixel-depth=16 --desktop-scalin$
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+DISPLAYOPTS="--video-scaling=100 --desktop-fullscreen=true --pixel-depth=16 --desktop-scaling=auto --window-close=ignore --speaker=disabled --microphone=disabled --pulseaudio=no"
 
 echo "Terminating any existing live view sessions"
-$DIR/terminateLiveViewSessions.sh 
+$DIR/terminateLiveViewSessions.sh
 
 echo "Restart VirtualHere connection to devices"
 $DIR/restartVhClient.sh
@@ -13,8 +13,25 @@ $DIR/restartVhClient.sh
 echo "Print VirtualHere Server Log"
 cat $DIR/vhserver.log
 
-echo "Starting Live View"
+adb start-server
+SLEEP=10
 
+$DIR/vhclientx86_64 -t LIST
+
+DEVICESCOUNT=$(expr $(adb devices | tee >(wc -l) | tail -1) - 2)
+
+while [ $DEVICESCOUNT -le 0 ]
+do
+	echo "Retry - Number of devices connected: $DEVICESCOUNT"
+	adb kill-server
+	sleep $SLEEP
+	adb start-server
+	DEVICESCOUNT=$(expr $(adb devices | tee >(wc -l) | tail -1) - 2)
+done
+
+echo "Number of devices connected: $DEVICESCOUNT"
+
+echo "Start Live View Sessions"
 # Mi 3
 xpra start --bind-tcp=0.0.0.0:14500 --html=on --start="scrcpy -s MYSERIALNUMBER -m 768" $DISPLAYOPTS --daemon=no :100 &
 sleep $SLEEP
