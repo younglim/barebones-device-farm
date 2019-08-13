ada1bb467f06c8185d6e4326bd404e572a82b264
younglim <younglim@users.noreply.github.com>
2019-08-13 17:38:05 +0800

    Get full directory of vhclient

--- restartVhClient.sh
+++ restartVhClient.sh
@@ -2,8 +2,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 echo "$DIR"
 
-sudo pkill -9 vhclientx86_64
-wait $(pgrep vhclientx86_64)
+vhclientx86_64=$(which vhclientx86_64)
 
-sudo $(which vhclientx86_64) -c $DIR/.vhui -l $DIR/vhserver.log &
+sudo pkill -9 $vhclientx86_64
+wait $(pgrep $vhclientx86_64)
+
+sudo $vhclientx86_64 -c $DIR/.vhui -l $DIR/vhserver.log &
 echo "VirtualHere is running at $"
