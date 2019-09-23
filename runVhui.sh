fea4294c303ce2c12f6a628d7a4907e8299685e9
younglim <younglim@users.noreply.github.com>
2019-09-23 14:08:19 +0800

    Dynamically load vhci-hcd and change port number of ui display

--- runVhui.sh
+++ runVhui.sh
@@ -3,5 +3,6 @@ sudo pkill -9 vhclientx86_64
 sudo pkill -9 vhuit64
 sleep 5
 
-sudo xpra start --bind-tcp=0.0.0.0:13000 --html=on --start="$DIR/vhuit64 -c $DIR/.vhui" --daemon=no :200
-
+sudo modprobe vhci-hcd
+sudo iptables -A INPUT -p tcp --dport 8083 -j ACCEPT
+sudo xpra start --bind-tcp=0.0.0.0:8083 --html=on --start="vhuit64 -c $DIR/.vhui" --daemon=no :20
