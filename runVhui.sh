2fef6c5c97888ffd1eec7ee6d1bbd3266aad0fad
younglim <younglim@users.noreply.github.com>
2019-09-23 15:27:02 +0800

    Fix typo for display port number

--- runVhui.sh
+++ runVhui.sh
@@ -5,4 +5,4 @@ sleep 5
 
 sudo modprobe vhci-hcd
 sudo iptables -A INPUT -p tcp --dport 8083 -j ACCEPT
-sudo xpra start --bind-tcp=0.0.0.0:8083 --html=on --start="vhuit64 -c $DIR/.vhui" --daemon=no :20
+sudo xpra start --bind-tcp=0.0.0.0:8083 --html=on --start="vhuit64 -c $DIR/.vhui" --daemon=no :200
