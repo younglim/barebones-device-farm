313c405356876ebee38e7be84ed37d1934554fa5
younglim <younglim@users.noreply.github.com>
2019-10-07 16:56:24 +0800

    Install linux kernel modules on ubuntu

--- restartVhClient.sh
+++ restartVhClient.sh
@@ -7,6 +7,19 @@ vhclientx86_64=$(which vhclientx86_64)
 sudo pkill -9 vhclientx86_64
 wait $(pgrep vhclientx86_64)
 
+if [[ $(which dpkg-query) ]]; then
+    echo "Ubuntu"
+
+    dpkg-query -l linux-modules-extra-$(uname -r)
+    package_installed_error=$?
+
+    if [ $package_installed_error -eq 1 ]; then
+    	sudo apt-get install -y linux-modules-extra-$(uname -r)
+    fi
+else
+    echo "RedHat"
+fi
+
 sudo modprobe vhci-hcd
 sudo $vhclientx86_64 -c $DIR/.vhui -l $DIR/vhserver.log &
 echo "VirtualHere is running at $"
