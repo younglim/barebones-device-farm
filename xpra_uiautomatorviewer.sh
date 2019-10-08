47ccd16df5f48a546a40fcd566a707f95cb9752f
younglim <younglim@users.noreply.github.com>
2019-10-08 23:46:47 +0800

    Close all uiautomatorviewer windows

--- xpra_uiautomatorviewer.sh
+++ xpra_uiautomatorviewer.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ $(which dpkg-query) ]]; then
+if [ -x "$(command -v dpkg-query)" ]; then
     echo "Ubuntu"
 
     dpkg-query -l wmctrl
@@ -13,21 +13,21 @@ else
     echo "RedHat"
 fi
 
+
 export PATH=/opt/android-sdk/tools/bin:$PATH
 DISPLAY=":201"
 
 xpra start --bind-tcp=0.0.0.0:13000 --html=on --start-child="uiautomatorviewer $DISPLAY" --dpi=96 --speaker=disabled --microphone=disabled --window-close=disconnect --dbus-proxy=no --dbus-control=no --encoding=rgb --compressors=lz4 --daemon=no --speaker=off $DISPLAY &
 
-# uiautomatorviewer &
 UIAUTOMATORVIEWER_PID=$!
 
-trap "kill $UIAUTOMATORVIEWER_PID" SIGINT SIGTERM
+trap "kill $UIAUTOMATORVIEWER_PID; exit" SIGINT SIGTERM
 
 UIAUTOMATORVIEWER_WINDOW=""
 
 while [ -z "$UIAUTOMATORVIEWER_WINDOW" ] ; do
   UIAUTOMATORVIEWER_WINDOW=$( wmctrl -l | grep "UI Automator" | awk '{print $1;}')
-  sleep 0.1
+  sleep 1
 done
 
 echo "Found UIAutomatorViewer Window $UIAUTOMATORVIEWER_WINDOW"
